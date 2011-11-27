97fa6cc346933c1d1cf38357fdfd82695bb1990a
Wong Liang Zan <zan@liangzan.net>
2011-11-27 11:33:30 +0800

    using the modular style

--- lib/broadcast_mode.rb
+++ lib/broadcast_mode.rb
@@ -1,6 +1,72 @@
-require File.expand_path(File.dirname(__FILE__) + '/metric')
+require "rubygems"
+require "bundler/setup"
+require "sinatra/base"
+require "json"
+$:.unshift File.dirname(__FILE__)
+require 'metric'
+require 'authentication'
 Dir[File.dirname(__FILE__) + "/metrics/*.rb"].each {|file| require file }
 
 module BroadcastMode
+  class Agent < Sinatra::Base
+    before do
+      if !BroadcastMode::Authentication.from_authorized_host?(request)
+        request.path_info = "/unauthorized"
+      end
+    end
+
+    get "/" do
+      erb :index
+    end
+
+    get "/unauthorized" do
+      'Unauthorized request'
+    end
+
+    get "/load.json" do
+      content_type :json
+      LoadMetric.current_state.to_json
+    end
+
+    get "/memory.json" do
+      content_type :json
+      MemoryMetric.current_state.to_json
+    end
+
+    get "/swap.json" do
+      content_type :json
+      SwapMetric.current_state.to_json
+    end
+
+    get "/disk_space.json" do
+      content_type :json
+      DiskSpaceMetric.current_state.to_json
+    end
+
+    get "/disk_io.json" do
+      content_type :json
+      DiskIOMetric.current_state.to_json
+    end
+
+    get "/users.json" do
+      content_type :json
+      UserMetric.current_state.to_json
+    end
+
+    get "/cpu.json" do
+      content_type :json
+      CPUMetric.current_state.to_json
+    end
+
+    get "/process.json" do
+      content_type :json
+      ProcessMetric.current_state.to_json
+    end
+
+    get "/network_io.json" do
+      content_type :json
+      NetworkIOMetric.current_state.to_json
+    end
+  end
 end
 
